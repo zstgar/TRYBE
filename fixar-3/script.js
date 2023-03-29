4f049d8e077a37780792b04a0f6e38959ae27d0a
zstgar <lucaszstgar@gmail.com>
2023-03-28 23:53:39 -0300

    Subindo dados de fundamentos/3.1

---
+++ fixar-3/script.js
@@ -0,0 +1,8 @@
+const grade = 80;
+if (grade >= 80) {
+  console.log("Parabéns, você faz parte do grupo de pessoas aprovadas!");
+} else if (grade < 80 && grade >= 60) {
+  console.log("Você está na nossa lista de espera.");
+} else if (grade < 60) {
+  console.log("Infelizmente, você reprovou.");
+}
