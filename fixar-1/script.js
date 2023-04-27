da717bd4b98e4bd0745bb532b5085cbb03c92633
zstgar <lucaszstgar@gmail.com>
2023-04-27 16:14:55 -0300

    Subindo dados de fundamentos/6.1

--- fixar-1/script.js
+++ fixar-1/script.js
@@ -22,16 +22,50 @@ button.addEventListener('click', () => {
 });
 
 const showPromo = (name, number) => {
-  const firstText = document.querySelector('#text-initial');
-  const secondText = document.querySelector('#text-final');
-  const productObject = checkNumber(parseInt(number));
+    const firstText = document.querySelector('#text-initial');
+    const secondText = document.querySelector('#text-final');
+    
+    try {
+      checkName(name);
+      checkNumber(parseInt(number));
+      checkValidRange(number);
+      const productObject = checkPromo(number);
+  
+      firstText.innerHTML = `Boas-vindas, ${name}!`;
+      secondText.innerHTML = `A promoção do dia é: 
+        ${productObject.product} no valor de R$ ${productObject.price}`;
+    } catch(err) {
+      secondText.innerHTML = err.message;
+    }
+  }
 
-  firstText.innerHTML = `Boas-vindas, ${name}!`;
-  secondText.innerHTML = `A promoção do dia é: 
-    ${productObject.product} no valor de R$ ${productObject.price}`;
-}
+  const checkName = (name) => {
+    let letters = /[aA-zZ]+/;
+    
+    if (!name.match(letters)) {
+      throw new Error('É necessário digitar um nome válido');
+    }
+  }
 
 // Segunda parte
 const checkNumber = (number) => {
-  return promo[number - 1];
-}
+    if (isNaN(number)) {
+      throw new Error('É necessário digitar um número');
+    }
+  }
+
+  // Terceira parte
+const checkPromo = (number) => {
+    for (let index = 0; index < promo.length; index += 1) {
+      if (number - 1 === index) {
+        return promo[index];
+      }
+    }
+  }
+
+  // Quarta parte
+const checkValidRange = (number) => {
+    if (number < 1 || number > 10) {
+      throw new Error('É necessário digitar um número entre 1 e 10');
+    }
+  }
