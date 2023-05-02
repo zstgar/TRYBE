b90367df316c1193ab2daad45e4b6e439bd80aa6
zstgar <lucaszstgar@gmail.com>
2023-05-01 21:47:18 -0300

    Subindo dados de fundamentos/6.1

---
+++ exercicio-3/script.js
@@ -0,0 +1,28 @@
+const validateInfo = (name, age) => {
+    if (!name || !age) {
+      throw new Error("Todas as informações são necessárias");
+    }
+  };
+  
+  const validateLegalAge = (age) => {
+    if (age < 18) {
+      throw new Error(
+        "Ops, infelizmente nesse momento você não pode fazer as aulas"
+      );
+    }
+  };
+  
+  const studentRegister = (name, age) => {
+    try {
+      validateInfo(name, age);
+      validateLegalAge(age);
+      return `${name}, boas-vindas à AuTrybe!`;
+    } catch (error) {
+      return error.message;
+    }
+  };
+  
+  console.log(studentRegister("Carlos", 18)); // Fluxo completo
+  console.log(studentRegister("Carlos", 12)) // Fluxo de exceção
+  console.log(studentRegister("Renan")) // Fluxo de exceção
+  
