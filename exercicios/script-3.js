b69602cb203ab22802e8658f917de6784d09c794
zstgar <lucaszstgar@gmail.com>
2023-05-22 08:58:27 -0300

    Subindo dados de fundamentos/7.5

---
+++ exercicios/script-3.js
@@ -0,0 +1,6 @@
+const greet = (name, msg) => `${msg} ${name}`;
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
---
+++ exercicios/script.js
@@ -0,0 +1,3 @@
+const myList = [5, 2, 3];
+
+const swap = ([a, b, c]) => [c, b, a];
